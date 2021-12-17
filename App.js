be1889206373bf5fb97078f2c0fb8678b49aa2b7
Marco Penajoia <penajoia@MPenajoias-MBP.local>
2021-12-16 20:55:33 -0800

    successfully connected to my local backend via GET

--- App.js
+++ App.js
@@ -1,13 +1,27 @@
 import React from 'react';
-import {View, Text, Image, StyleSheet} from 'react-native';
+import {View, Text, Image, StyleSheet, Button} from 'react-native';
 import Header from './components/Header';
 import apiUrl from './backendAPI/apiConfig';
 import { globalStyles } from './styles/global';
+
+
+// GET
+const getAssets = () => {
+  fetch('http://localhost:4000/assets', {
+    method: 'GET'}
+  )
+  .then((res) => res.json())
+  .then((data) => console.log(data))
+}
+const handleButton = () => {
+  getAssets()
+}
 
 const App = () => {
   return(
     <View style={globalStyles.container}>
       <Header title='Shopping List'/>
+      <Button onPress={handleButton} title='Hey'></Button>
     </View>
   )
 }
