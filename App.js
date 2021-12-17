9db79bfbb12f765e7608abf01f0d7ba15f641c93
Marco Penajoia <penajoia@MPenajoias-MBP.local>
2021-12-16 20:35:27 -0800

    global styles attempted

--- App.js
+++ App.js
@@ -2,33 +2,13 @@ import React from 'react';
 import {View, Text, Image, StyleSheet} from 'react-native';
 import Header from './components/Header';
 import apiUrl from './backendAPI/apiConfig';
+import { globalStyles } from './styles/global';
 
 const App = () => {
   return(
-    <View style={styles.container}>
+    <View style={globalStyles.container}>
       <Header title='Shopping List'/>
-      {/* <Text style={styles.text}>Hello World</Text> */}
-      {/* <Image source={{uri: 'https://randomuser.me/api/portraits/men/1.jpg'}} style={styles.img} /> */}
     </View>
   )
 }
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: 60,
-  //   justifyContent: 'center',
-  //   alignItems: 'center'
-  // },
-  // text: {
-  //   color: '#c2c2c2',
-  //   fontSize: 30
-  // },
-  // img: {
-  //   width: 100,
-  //   height: 100,
-  //   borderRadius: 100 / 2,
-  }
-});
-
-
 export default App;
--- backendAPI/apiConfig.js
+++ backendAPI/apiConfig.js
@@ -1,17 +1,18 @@
-let apiUrl
-const apiUrls = {
-  // your production api url
-  production: 'https://immense-badlands-54158.herokuapp.com',
-  // your development api url
-  development: 'http://localhost:4000'
-}
+// let apiUrl
+// const apiUrls = {
+//   // your production api url
+//   production: 'https://immense-badlands-54158.herokuapp.com',
+//   // your development api url
+//   development: 'http://localhost:4000'
+// }
 
-// check if development or production
-if (window.location.hostname === 'localhost') {
-  apiUrl = apiUrls.development
-} else {
-  apiUrl = apiUrls.production
-}
+// // check if development or production
+// // window.location.hostname does not work in mobile app?
+// if (window.location.hostname === 'localhost') {
+//   apiUrl = apiUrls.development
+// } else {
+//   apiUrl = apiUrls.production
+// }
 
-// export api url
-export default apiUrl
+// // export api url
+// export default apiUrl;
