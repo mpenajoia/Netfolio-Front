6a6dfae3810b0eee5a58f5c8ee06eedfb07aeedf
Marco Penajoia <penajoia@MPenajoias-MBP.local>
2021-12-19 16:42:09 -0800

    Ind Asset got props correctly

--- components/Asset.js
+++ components/Asset.js
@@ -5,16 +5,45 @@ import { Button } from 'react-native-elements';
 
 const Asset = (props) => {
     // make component that will list the asset's info plus the edit and delete button
+    let asset = props.assets
+    let ind
+    let cat
+    let qty 
+    let sym 
+    let name 
+    let invested 
+    let live 
+    let id 
+    if(asset){
+        ind = props.index
+        cat = props.item.category
+        qty = props.item.qty
+        sym = props.item.symbol
+        name = props.item.name
+        invested = props.item.invested
+        live = props.item.current
+        id = props.item._id
+    }else{
+        ind = 'loading'
+        cat = 'loading'
+        qty = 'loading'
+        sym = 'loading'
+        name = 'loading'
+        invested = 'loading'
+        live = 'loading'
+        id = 'loading'
+    }
 
-
+    
 
     return(
-        <View>
+        <View key={props.index}>
             <Card>
                 <Text>
+                    {name} 
                     
-                    Indv Asset
-                    <Button title="Clear button" type="clear" />
+                    <Button title="Edit" buttonStyle={{fontSize: 20, backgroundColor: '#000', marginHorizontal: 5}} />
+                    <Button title="Delete" buttonStyle={{fontSize: 20}} />
                 </Text>
             </Card>
         </View>
--- components/Assets.js
+++ components/Assets.js
@@ -1,21 +1,25 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, ScrollView} from 'react-native';
 import Asset from './Asset';
 
 const Assets = (props) => {
 
     let assetsMap
     if (props.assets){
-        assetsMap = props.assets.map((item, index) => <Text key={index}>{item.name}, {item.symbol}, {item.qty}, {item._id}</Text>)
+        // this map function should map through the array of objects and return the Asset component, instead of a Text component, with the information of each Asset
+        assetsMap = props.assets.map((item, index) => <Asset key={index} assets={props.assets} index={index} item={item}/>)
     }else{
         assetsMap = <Text>Map empty</Text>
     }
-
+    
+    // <Text key={index}>{item.name}, {item.symbol}, {item.qty}, {item._id}</Text>
     return(
         <View>
+            {/* Put Add Asset Button here */}
             <Text>Assets Page</Text>
+            <ScrollView>
             {assetsMap}
-            {/* <Asset/> */}
+            </ScrollView>
         </View>
     )
 }
