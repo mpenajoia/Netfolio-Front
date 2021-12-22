08c40e61f75cd8978b630cd9f91c4c9f344789a9
Marco Penajoia <penajoia@MPenajoias-MBP.local>
2021-12-22 15:35:48 -0800

    babel file changed

--- babel.config.js
+++ babel.config.js
@@ -1,15 +1,4 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  plugins: [
-    [
-      'module-resolver',
-      {
-        extensions: ['.tsx', '.ts', '.js', '.json'],
-        alias: {
-          [pak.name]: path.join(__dirname, '..', pak.source),
-        },
-      },
-    ],
-    'react-native-reanimated/plugin'
-  ]
+
 };
